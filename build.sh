d721b0e28fec7bc4f2908390b4ff1654db8e3d37
Your Name <you@example.com>
2023-07-02 11:03:40 -0300

    build with date "as" version

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-pyinstaller --noconfirm --onefile --windowed --icon "./icon.ico" --name "requester" --clean --add-data "./icon.png;."  "./main.py"
+pyinstaller --noconfirm --onefile --windowed --icon "./icon.ico" --name "requester_`date +%Y%m%d`" --clean --add-data "./icon.png;."  "./main.py"
+
+#rm -rf build requester*.spec
